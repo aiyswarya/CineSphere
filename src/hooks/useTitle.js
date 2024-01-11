dcb7a289fd1b80c7584301a2e1ee2d11787e14ab
aiyswarya <aiyshlaksh@gmail.com>
2024-01-11 05:25:19 -0800

    Cinesphere files

--- src/hooks/useTitle.js
+++ src/hooks/useTitle.js
@@ -1,10 +1,9 @@
-import { useEffect } from "react"
+import { useEffect } from "react";
 
 export const useTitle = (title) => {
+  useEffect(() => {
+    document.title = `${title} / CineSphere`;
+  });
 
-    useEffect(() =>{
-        document.title = `${title} / Cinemate`
-      })
-
-  return null
-}
+  return null;
+};
